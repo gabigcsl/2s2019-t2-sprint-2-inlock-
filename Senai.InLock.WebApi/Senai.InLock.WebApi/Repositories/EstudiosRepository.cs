f0373907e2ed3ee8e8ad49d6b77e58f386116b07
João Victor Soncini Santos <44634488833@info.local>
2019-09-02 16:24:45 -0300

    up

---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/EstudiosRepository.cs
@@ -0,0 +1,55 @@
+﻿using Senai.InLock.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Repositories
+{
+    public class EstudiosRepository
+    {
+        public List<Estudios> Listar ()
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                return ctx.Estudios.ToList();
+            }
+        }
+
+        public void Cadastrar (Estudios estudios)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                ctx.Estudios.Add(estudios);
+            }
+        }
+
+        public Estudios BuscarPorId(int id)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                return ctx.Estudios.FirstOrDefault(x => x.IdEstudio == id);
+            }
+        }
+
+        public void Deletar (int id)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                Estudios estudios = ctx.Estudios.Find(id);
+                ctx.Estudios.Remove(estudios);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Atualizar (Estudios estudios)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                Estudios estudioBuscado = ctx.Estudios.FirstOrDefault(x => x.IdEstudio == estudios.IdEstudio);
+                ctx.Estudios.Update(estudios);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/JogoRepository.cs
@@ -0,0 +1,57 @@
+﻿using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Repositories
+{
+    public class JogoRepository
+    {
+        public List<Jogos> Listar()
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                return ctx.Jogos.ToList();
+            }
+        }
+
+        public void Cadastrar(Jogos jogos)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                ctx.Jogos.Add(jogos);
+            }
+        }
+
+        public Jogos BuscarPorId(int id)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                return ctx.Jogos.FirstOrDefault(x => x.IdJogos == id);
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                Jogos jogos = ctx.Jogos.Find(id);
+                ctx.Jogos.Remove(jogos);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Jogos jogos)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                Jogos JogoBuscado = ctx.Jogos.FirstOrDefault(x => x.IdJogos == jogos.IdJogos);
+                ctx.Jogos.Update(JogoBuscado);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
